c645f7202947150fa37ad0255e6b151db786ae40
djunnni <djunnni@gmail.com>
2022-11-15 16:46:23 +0900

    config: eslintrc, prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,20 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        bracketSameLine: true,
+        singleQuote: false,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: "all",
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: "always",
+        endOfLine: "auto",
+        useTabs: false,
+      },
+    ],
   },
 };
