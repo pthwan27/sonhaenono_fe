d5538820a723b4fd456a34a8ac532f2239173ffa
Dongjoon Lee <djunnni@gmail.com>
2022-11-19 11:16:44 +0900

    feat: kakao 지도 추가

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,5 +1,5 @@
 export const REMEBMER_USER_ID_KEY = "remember.user.id";
-
+export const KAKAO_MAP_API_KEY = "1f793ce9f35193629bdf177ed4a517b9";
 export default {
   REMEBMER_USER_ID_KEY,
 };
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>MapView</div>
+  <div id="map"></div>
 </template>
 
 <script>
+import { KAKAO_MAP_API_KEY } from "@/common/constant";
 export default {
   name: "MapView",
+  methods: {
+    initMap() {
+      var mapContainer = document.getElementById("map"),
+        mapOption = {
+          center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+      /* eslint-disable */
+      var map = new kakao.maps.Map(mapContainer, mapOption);
+    },
+  },
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${KAKAO_MAP_API_KEY}`;
+      document.head.appendChild(script);
+    }
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
